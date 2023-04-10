478dd764f0214b1fc3d0a34d5282ab56544beeb8
mcgraneder <mcgrane480@gmail.com>
2023-04-10 04:11:39 +0100

    first2

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import webpack from "webpack"
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
+
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
 /** @type {import('next').NextConfig} */
